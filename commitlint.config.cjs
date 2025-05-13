7564dc13ff1ead52bf0ccf1b7af144766f120a82
avelovecode <14501970+avelovecode@user.noreply.gitee.com>
2025-05-13 21:35:03 +0800

    feat: test commitlint fix

---
+++ commitlint.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'subject-case': [0],
+  },
+}
